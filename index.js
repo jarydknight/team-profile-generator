4b48153ff7fadbf8d299ba91f42bf682337f23e8
Jaryd Knight <jaryd.knight@hotmail.com>
2022-07-13 17:18:45 -0400

    complete prompts for information

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
+// const buildTeam = require("./utils/buildTeam");
+const managerQuestions = require("./utils/managerQuestions");
+const engineerQuestions = require("./utils/engineerQuestions");
+const employeeQuestions = require("./utils/employeeQuestions");
+const internQuestions = require("./utils/internQuestions");
 const inquirer = require("inquirer");
-const fs = require("fs");
-const buildIndividualMember = require("./utils/buildIndividualMember");
+
+const continueQuestion = [{
+    type: "confirm",
+    name: "continueBuilding",
+    message: "Would you like to add members to your team?",
+}];
+
+const continueAddingMembers = () => {
+    inquirer.prompt(continueQuestion)
+    .then((data) => {
+        if (data.continueBuilding) {
+            buildTeam();
+        }
+    })
+};
+
+const buildIndividualMember = (data) => {
+    switch (data.member) {
+        case "Manager":
+            inquirer.prompt(managerQuestions)
+            .then(continueAddingMembers);
+            break;
+
+        case "Engineer":
+            inquirer.prompt(engineerQuestions)
+            .then(continueAddingMembers);
+            break;
+
+        case "Employee":
+            inquirer.prompt(employeeQuestions)
+            .then(continueAddingMembers);
+            break;
+        
+        case "Intern":
+            inquirer.prompt(internQuestions)
+            .then(continueAddingMembers);
+            break;
+    };
+};
 
 const startQuestion = [{
     type: "list",
     name: "member",
     message: "Choose the type of employee you wish to add to your team",
     choices: ["Manager", "Engineer", "Employee", "Intern"],
-}]
+}];
 
-
-
-const startBuildingTeam = () => {
+const buildTeam = () => {
     inquirer.prompt(startQuestion)
     .then(buildIndividualMember)
+};
+
+const init = () => {
+    buildTeam()
 }
 
-startBuildingTeam();
+init();
